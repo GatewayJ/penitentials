e50f94edf4af70e7b2064d69566596c8165998cb
季宏伟 <835269233@qq.com>
2020-03-09 20:10:24 +0800

    安装axios

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from "./router"
 import App from './App.vue'
 import '../src/assets/font'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 
+Vue.use(axios,VueAxios)
 Vue.use(ElementUI)
 
 new Vue({
